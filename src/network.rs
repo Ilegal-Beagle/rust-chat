8434c9763dae9cb42fe55b24e4443496f2bf25a1
Ilegal-Beagle <munoza204@gmail.com>
2025-11-14 11:33:52 -0600

    put client and server logic into functions

--- src/network.rs
+++ src/network.rs
@@ -1,9 +1,12 @@
 // network.rs
 use std::{
     str,
-    io::{prelude::*, Result},
-    net::{TcpStream, SocketAddr}, //, IpAddr, Ipv4Addr},
+    thread,
+    io::{prelude::*},
+    net::{TcpListener, TcpStream, SocketAddr}, //, IpAddr, Ipv4Addr},
     time::{Duration},
+    sync::mpsc::{Sender, Receiver},
+    error::{Error},
 };
 
 use crate::ui::chat::Message;
@@ -15,7 +18,7 @@ pub fn try_connect(address: &SocketAddr, timeout: Duration) -> bool {
     }
 }
 
-pub fn handle_connection(mut stream: TcpStream) -> Result<()>{
+pub fn handle_connection(mut stream: TcpStream) -> std::io::Result<()>{
     let mut buf = [0u8; 128]; // where the message recved is stored
     println!("client connected");
 
@@ -44,7 +47,7 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<()>{
 
                 println!("{}", deserialized_msg);
 
-                // send to serve
+                // send
                 match stream.write_all(message.as_bytes()) {
                     Ok(_) => {println!("message sent to client");},
                     Err(e) => {
@@ -63,4 +66,82 @@ pub fn handle_connection(mut stream: TcpStream) -> Result<()>{
 
     println!("client disconnected");
     Ok(())
+}
+
+pub fn server(socket: &SocketAddr) {
+    let listener = TcpListener::bind(&socket).expect("couldnt bind");
+    println!("Started listening on port 7878");
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+        thread::spawn(|| {
+            let _ = handle_connection(stream);
+        });
+    }
+}
+
+// does the client functions. gets a message from user and sends and reveives it back from server.
+pub fn client(socket: &SocketAddr, rx_ui: &Receiver<Message>, tx_net: &Sender<Message>) {
+    let mut stream = TcpStream::connect(&socket).expect("could not connect");
+    stream.set_read_timeout(Some(Duration::new(0, 10000))).expect("could not set timeout");
+    println!("connected to server.");
+
+    loop {
+        // get client message from UI
+        match rx_ui.try_recv() {
+            
+            Ok(msg) => {
+                // serialize message
+                let serialized_msg = match serde_json::to_string(&msg) {
+                    Ok(msg) => msg,                                
+                    Err(e) => {
+                        eprintln!("error in serializing message: {e:?}");
+                        continue;
+                    },
+                };
+                
+                // send message to server
+                match stream.write_all(serialized_msg.as_bytes()) {
+                    Ok(_) => {println!("message sent to server")},
+                    Err(e) => {println!("Error in sending message: {e:?}")},
+                };
+
+                // receive repsonse from server
+                let msg = match get_message(&mut stream) {
+                    Ok(msg) => msg,
+                    Err(e) => {
+                        println!("Error: {e}");
+                        continue;
+                    },
+                };
+
+                match tx_net.send(msg) {
+                    Ok(_) => {},
+                    Err(e) => {
+                        eprintln!("Error sending message: {e}");
+                        continue;
+                    }
+                }
+            },
+
+            Err(_) => {/* Do nothing if nothing was recved */},
+        }
+        
+    }
+}
+
+// gets message from TCP, converts it to Message and returns it
+pub fn get_message(stream: &mut TcpStream) -> Result<Message, Box<dyn Error>> {
+    let mut buf = [0u8; 128];
+    let bytes_read = stream.read(&mut buf)?;
+    
+    // UTF8 to str
+    let message = str::from_utf8(&buf[..bytes_read])?;
+
+    // str to Message
+    let deserialized_msg: Message = serde_json::from_str(&message)?;
+
+    println!("{}", deserialized_msg);
+
+    Ok(deserialized_msg)
 }
--- src/ui/chat.rs
+++ src/ui/chat.rs
@@ -5,13 +5,12 @@ use std::{
     thread,
     fmt,
     io::{prelude::*},
-    net::{TcpListener, TcpStream, SocketAddr, IpAddr, Ipv4Addr},
+    net::{SocketAddr, IpAddr, Ipv4Addr},
     time::{Duration},
     sync::{mpsc},
 };
 
 use serde::{Serialize, Deserialize};
-use serde_json;
 
 pub struct App {
     user_name: String,
@@ -80,93 +79,14 @@ impl App {
 
             // create a server
             thread::spawn( move || {
-                let listener = TcpListener::bind(&SOCKET).expect("couldnt bind");
-                println!("Started listening on port 7878");
-                
-                for stream in listener.incoming() {
-                    let stream = stream.unwrap();
-                    thread::spawn(|| {
-                        let _ = network::handle_connection(stream);
-                    });
-                }
-            } );
+                network::server(&SOCKET);
+            });
             
 
         } else {
-
             // create a client
             thread::spawn(move || {
-                let mut stream = TcpStream::connect(&SOCKET).expect("could not connect");
-                stream.set_read_timeout(Some(Duration::new(0, 10000))).expect("could not set timeout");
-                let mut buf = [0u8; 128];
-                println!("connected to server.");
-
-                loop {
-                    // get client message from UI
-                    match rx_ui.try_recv() {
-                        
-                        Ok(msg) => {
-                            // serialize message
-                            let serialized_msg = match serde_json::to_string(&msg) {
-                                Ok(msg) => msg,                                
-                                Err(e) => {
-                                    eprintln!("error in serializing message: {e:?}");
-                                    continue;
-                                },
-                            };
-                            
-                            // send message to server
-                            match stream.write_all(serialized_msg.as_bytes()) {
-                                Ok(_) => {println!("message sent to server")},
-                                Err(e) => {println!("Error in sending message: {e:?}")},
-                            };
-
-                            // receive repsonse from server
-                            match stream.read(&mut buf) {
-                                Ok(0) => break,
-
-                                Ok(bytes_read) => {
-                                    // UTF8 to str
-                                    let message = match str::from_utf8(&buf[..bytes_read]) {
-                                        Ok(msg) => msg,
-                                        Err(e) => {
-                                            eprintln!("Invalid UTF8: {e}");
-                                            continue;
-                                        },
-                                    };
-
-                                    // str to Message
-                                    let deserialized_msg: Message = match serde_json::from_str(&message) {
-                                        Ok(msg) => msg,
-                                        Err(e) => {
-                                            eprintln!("Could not deserialize message: {e}");
-                                            continue;
-                                        },
-                                    };
-
-                                    match tx_net.send(deserialized_msg) {
-                                        Ok(_) => {},
-                                        Err(e) => {
-                                            eprintln!("Error sending message: {e}");
-                                            continue;
-                                        }
-                                    }
-
-                                },
-
-                                Err(e) => {
-                                    eprintln!("Error receiving message: {e}");
-                                    continue;
-                                },
-
-                            };
-
-                        },
-
-                        Err(_) => {/* Do nothing if nothing was recved */},
-                    }
-                    
-                }
+                network::client(&SOCKET, &rx_ui, &tx_net);
             } );
         }
 
